e2250d35302519e5f1eaafd3d4e6d0f31fc8d8d3
jaypea07 <jaypea07@gmail.com>
2017-06-17 18:25:58 -0500

    Moving the ability scores into an array and iterating over it during render

--- src/character/character.tsx
+++ src/character/character.tsx
@@ -1,44 +1,65 @@
 import * as React from 'react';
 
 export class Character extends React.Component<{}, void> {
+  private abilityScores: Array<any>;
+
+  componentWillMount() {
+    this.abilityScores = [
+      {
+        name: 'Strength',
+        score: 13,
+        modifier: 1,
+      },
+      {
+        name: 'Dexterity',
+        score: 13,
+        modifier: 1,
+      },
+      {
+        name: 'Constituion',
+        score: 13,
+        modifier: 1,
+      },
+      {
+        name: 'Intelligence',
+        score: 13,
+        modifier: 1,
+      },
+      {
+        name: 'Wisdom',
+        score: 13,
+        modifier: 1,
+      },
+      {
+        name: 'Charisma',
+        score: 13,
+        modifier: 1,
+      }
+    ];
+  }
+
   render() {
+
+    const statList = this.abilityScores.map(ability => {
+      return  <tr>
+                <td>{ability.name}</td>
+                <td>{ability.score}</td>
+                <td>{ability.modifier}</td>
+              </tr>;
+    });
+
+    
+
     return (
       <section>
         <h2>Dwarven Wizard</h2>
 
-        <table>
-          <tr>
-            <td>Strength</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-          <tr>
-            <td>Dexterity</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-          <tr>
-            <td>Constituion</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-          <tr>
-            <td>Intelligence</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-          <tr>
-            <td>Wisdom</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-          <tr>
-            <td>Charisma</td>
-            <td>13</td>
-            <td>(+1)</td>
-          </tr>
-        </table>
+        <table>{statList}</table>
       </section>
     );
   }
+
+  // private rollD20() {
+  //   return Math.floor(Math.random() * 19 + 1);
+  // }
 }
