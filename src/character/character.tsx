fd924a8f16e53eda61348e3381b4be5c7bee6b9f
jaypea07 <jaypea07@gmail.com>
2017-06-17 20:51:23 -0500

    Moving most of the logic into the app component so that I can use it later to re-roll with the button

--- src/character/character.tsx
+++ src/character/character.tsx
@@ -2,24 +2,19 @@ import * as React from 'react';
 import { CONSTANTS } from '../core/constants';
 import { AbilityScore } from './ability-score.model';
 
-export class Character extends React.Component<{}, void> {
-  private abilityScores: Array<AbilityScore>;
+export interface CharacterProps {
+  stats: Array<AbilityScore>;
+}
 
-  componentWillMount() {
-    this.abilityScores = this.rollStats(CONSTANTS.REQUIRED_STATS);
-
-    while (this.isNotHeroic(this.abilityScores)) {
-      this.abilityScores = this.rollStats(CONSTANTS.REQUIRED_STATS);
-    }
-  }
+export class Character extends React.Component<CharacterProps, void> {
 
   render() {
-    const statList = this.abilityScores.map(ability => {
+    const statList = this.props.stats.map(stat => {
       return (
-        <tr key={ability.name}>
-          <td>{ability.name}</td>
-          <td>{ability.score}</td>
-          <td>({ability.modifier})</td>
+        <tr key={stat.name}>
+          <td>{stat.name}</td>
+          <td>{stat.score}</td>
+          <td>({stat.modifier})</td>
         </tr>
       );
     });
@@ -40,51 +35,5 @@ export class Character extends React.Component<{}, void> {
   private getRandomNumber(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min) + min);
   }
-
-  private rollStats(requiredStats: Array<string>): Array<AbilityScore> {
-    return requiredStats.map(abilityScore => {
-      return new AbilityScore(abilityScore, this.rollSingleStat());
-    });
-  }
-
-  // roll 4, drop the lowest
-  private rollSingleStat(): number {
-    let rolls: Array<number> = [];
-    let stat = 0;
-
-    for (let i = 0; i < 4; i++) {
-      rolls.push(this.rollD6());
-    }
-
-    rolls.sort().shift();
-    rolls.map(roll => {
-      stat += roll;
-    });
-
-    return stat;
-  }
-
-  private rollD6(): number {
-    return this.getRandomNumber(1, 6);
-  }
-
-  // 2 scores 14 or higher
-  // total score is 70 or higher
-  private isNotHeroic(abilityScores: Array<AbilityScore>): boolean {
-    let combinedTotal: number = 0;
-    let statsAboveThreshold: number = 0;
-
-    abilityScores.map(stat => {
-      combinedTotal += stat.score;
-      if (stat.score >= CONSTANTS.REQUIRED_STATS_THRESHOLD) {
-        ++statsAboveThreshold;
-      }
-    });
-
-    if (combinedTotal >= CONSTANTS.REQUIRED_STAT_TOTAL && 
-        statsAboveThreshold >= CONSTANTS.REQUIRED_STATS_ABOVE_THRESHOLD) {
-      return false;
-    }
-    return true;
-  }
+  
 }
--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,16 +1,79 @@
 import * as React from 'react';
+import { CONSTANTS } from '../core/constants';
 import { Character } from '../character/character';
+import { AbilityScore } from '../character/ability-score.model';
 import './app.css';
 
 class App extends React.Component<{}, null> {
+  private abilityScores: Array<AbilityScore>;
+  
+  componentWillMount() {
+    this.abilityScores = this.rollStats(CONSTANTS.REQUIRED_STATS);
+    while (this.isNotHeroic(this.abilityScores)) {
+      this.abilityScores = this.rollStats(CONSTANTS.REQUIRED_STATS);
+    }
+  }
+
   render() {
     return (
       <section>
         <h1>Dungeons &amp; Dragons Character Generator</h1>
         <h2>I'm going to roll a...</h2>
-        <Character />
+        <Character stats={this.rollStats(CONSTANTS.REQUIRED_STATS)} />
+        <button>That's dumb. Roll again!</button>
       </section>
     );
+  }
+
+  private rollStats(requiredStats: Array<string>): Array<AbilityScore> {
+    return requiredStats.map(abilityScore => {
+      return new AbilityScore(abilityScore, this.rollSingleStat());
+    });
+  }
+
+  // roll 4, drop the lowest
+  private rollSingleStat(): number {
+    let rolls: Array<number> = [];
+    let stat = 0;
+
+    for (let i = 0; i < 4; i++) {
+      rolls.push(this.rollD6());
+    }
+
+    rolls.sort().shift();
+    rolls.map(roll => {
+      stat += roll;
+    });
+
+    return stat;
+  }
+
+  private rollD6(): number {
+    return this.getRandomNumber(1, 6);
+  }
+
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  // 2 scores 14 or higher
+  // total score is 70 or higher
+  private isNotHeroic(abilityScores: Array<AbilityScore>): boolean {
+    let combinedTotal: number = 0;
+    let statsAboveThreshold: number = 0;
+
+    abilityScores.map(stat => {
+      combinedTotal += stat.score;
+      if (stat.score >= CONSTANTS.REQUIRED_STATS_THRESHOLD) {
+        ++statsAboveThreshold;
+      }
+    });
+
+    if (combinedTotal >= CONSTANTS.REQUIRED_STAT_TOTAL && 
+        statsAboveThreshold >= CONSTANTS.REQUIRED_STATS_ABOVE_THRESHOLD) {
+      return false;
+    }
+    return true;
   }
 }
 
